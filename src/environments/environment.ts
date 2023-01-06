fe7b4332dad14b6094c81e9d9956f7c99f3cef81
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-01-06 16:34:51 -0300

    client credentials em variável de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:8080',
@@ -9,15 +5,6 @@ export const environment = {
   logoutRedirectToUrl: 'http://127.0.0.1:4200/login',
   tokenAllowedDomains: [ /localhost:8080/ ],
   tokenDisallowedRoutes: [/\/oauth2\/token/],
-  clientCredentialsEncoded: 'Basic dG9kby13ZWJhcHA6RXhiR2Y3WnNCZ0VxdGZ4cUc5eEw='
+  clientCredentialsEncoded: `${process.env['CLIENT_CREDENTIALS']}`
   
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
