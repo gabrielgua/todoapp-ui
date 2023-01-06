e00cc78b76b53f756185628e60f7df281a2d4a0c
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-01-06 19:58:44 -0300

    atualizando dependências e client credentials estão em variável de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   logoutRedirectToUrl: 'http://127.0.0.1:4200/login',
   tokenAllowedDomains: [ /localhost:8080/ ],
   tokenDisallowedRoutes: [/\/oauth2\/token/],
-  clientCredentialsEncoded: `Basic dG9kby13ZWJhcHA6RXhiR2Y3WnNCZ0VxdGZ4cUc5eEw=`,
+  clientCredentialsEncoded: `${process.env['CLIENT_CREDENTIALS']}`
 };
