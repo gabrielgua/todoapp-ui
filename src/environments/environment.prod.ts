758bbb9687b1ea240bf03d16eaecb08f6b9838dd
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-01-06 21:39:17 -0300

    mudando url do app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   baseUrl: 'https://todoapp-api-production.up.railway.app',
-  oAuthCallBackUrl: 'https://todoapp-gabrielgua.vercel.app/authorized',
-  logoutRedirectToUrl: 'https://todoapp-gabrielgua.vercel.app/login',
+  oAuthCallBackUrl: 'https://todoapp-ui-ivory.vercel.app/authorized',
+  logoutRedirectToUrl: 'https://todoapp-ui-ivory.vercel.app/login',
   tokenAllowedDomains: [ /todoapp-api-production.up.railway.app/ ],
   tokenDisallowedRoutes: [/\/oauth2\/token/],
   clientCredentialsEncoded: 'Basic '+ process.env['CLIENT_CREDENTIALS']
